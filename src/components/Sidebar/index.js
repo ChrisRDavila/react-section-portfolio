df3defb6de3c3ffb182353eae9da03a568c559ce
ChrisRDavila <crossdavila@gmail.com>
2024-01-05 15:37:22 -0800

    update styling on nav bar, add links to a home, about and contact page, as well as links to github and linkedIn accounts

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,9 @@
 import './index.scss';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import logoR from '../../assets/images/logo-r.jpeg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const SideBar = () => {
   return (
@@ -9,6 +12,44 @@ const SideBar = () => {
         <img src={logoR} alt='logo-home-link' />
         <h1 className="sub-logo">ChrisRDavila</h1>
       </Link>
+      <nav>
+      <NavLink 
+        exact='true' 
+        activeclassname='active' 
+        to='/'>
+        <FontAwesomeIcon icon={faHome} color='4d4d4e' />
+      </NavLink>
+      <NavLink 
+      exact='true' 
+      activeclassname='active' 
+      className='about-link'
+      to='/about'>
+        <FontAwesomeIcon icon={faUser} color='4d4d4e' />
+      </NavLink>
+      <NavLink 
+        exact='true' 
+        activeclassname='active' 
+        className='contact-link' 
+        to='/contact'>
+        <FontAwesomeIcon icon={faEnvelope} color='4d4d4e' />
+      </NavLink>
+    </nav>
+      <ul>
+        <li>
+          <a target='_blank' 
+          rel='noreferrer' 
+          href='https://www.linkedin.com/in/christopher-davila-004408269/'>
+            <FontAwesomeIcon icon={faLinkedin} color='4d4d4e' />
+          </a>
+        </li>
+        <li>
+          <a target='_blank' 
+          rel='noreferrer' 
+          href='https://github.com/ChrisRDavila'>
+            <FontAwesomeIcon icon={faGithub} color='4d4d4e' />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 }
