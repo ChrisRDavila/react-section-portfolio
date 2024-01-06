435af35f3aa1523adc4a3e8133db7016b8812284
ChrisRDavila <crossdavila@gmail.com>
2024-01-05 17:34:14 -0800

    adding styling for home page, some issues targeting letter animations like rotate on R logo image

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,10 +1,19 @@
 import './index.scss';
+import HomeRLogo from '../../assets/images/logo-r.jpeg';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
     <div className='container home-page'>
         <div className='text-zone'>
-          <h1>hello</h1>
+          <h1>Hi, <br/> I'm</h1>
+          <img src={HomeRLogo} alt='home-logo-R' />
+          oss Davila
+          <br/>
+          <h2>Full-stack developer / Chef / Student / Musician</h2>
+            <Link to='/contact' className='flat-button'>
+            CONTACT ME
+            </Link>
         </div>
     </div>
   );
