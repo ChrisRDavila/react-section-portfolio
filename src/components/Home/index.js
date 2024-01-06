5d2261c7b6622d3aa2e794efa571aa816d9a65fd
ChrisRDavila <crossdavila@gmail.com>
2024-01-05 18:39:04 -0800

    create module to reuse for animating letters, implement it on Home page, as well as animating fadeInRight for opaque guitar logo that lines up with letter animation timing

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,24 +1,50 @@
-import './index.scss';
+import { Link} from 'react-router-dom';
 import LogoTitle from '../../assets/images/logo-r.jpeg';
-import { Link } from 'react-router-dom';
+import './index.scss';
+import AnimatedLetters from '../AnimatedLetters';
+import { useEffect, useState } from 'react'
+
 
 const Home = () => {
+  const [letterClass, setLetterClass] = useState('text-animate')
+  const nameArray = ['o','s','s', ' ', 'D', 'a', 'v', 'i', 'l', 'a']
+  const jobArray = ['w', 'e', 'b', ' ', 'd', 'e', 'v', 'e', 'l', 'o', 'p', 'e', 'r','.']
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLetterClass('text-animate-hover')
+    }, 4000)
+  }, [])
+
   return (
-    <div className='container home-page'>
+      <div className='container home-page'>
         <div className='text-zone'>
-          <h1>Hi, <br/> I'm</h1>
+          <h1>
+            <span className={letterClass}>H</span>
+            <span className={`${letterClass} _12`}>i, </span>
+            <br />
+            <span className={`${letterClass} _13`}> I</span>
+            <span className={`${letterClass} _14`}>'m</span>
+
           <img 
           src={LogoTitle} 
           alt='developer' 
           />
-          <p>oss Davila</p>
-          <br/>
-          <h2>Full-stack developer / Chef / Student / Musician</h2>
-            <Link to='/contact' className='flat-button'>
+          <AnimatedLetters letterClass={letterClass} 
+          strArray={nameArray} 
+          idx={15} />
+          <br />
+          <AnimatedLetters letterClass={letterClass} 
+          strArray={jobArray} 
+          idx={26} />
+          </h1>
+          <h2>Full-stack developer / Student / Musician</h2>
+          <Link to='/contact' className='flat-button'>
             CONTACT ME
             </Link>
         </div>
-    </div>
+        
+      </div>
   );
 }
 
