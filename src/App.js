65ec4afabd160d0c9556ee76a134b7be8e37f37d
ChrisRDavila <crossdavila@gmail.com>
2024-01-05 16:16:34 -0800

    settin gup layout for main page, will have tags to look like code and guitar fiant in background

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import Home from './components/Home';
 
 function App() {
   return (
     <>
       <Routes>
         <Route path="/" element={<Layout />} />
-        {/* <Route path="/about" element={<About />} /> */}
+          <Route index element={<Home />} />
       </Routes>
     </>
   );
--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,11 +1,27 @@
 import './index.scss';
 import SideBar from '../Sidebar';
+import { Outlet } from 'react-router-dom';
+import Guitar from './Guitar';
 
 const Layout = () => {
   return (
-    <>
-      <SideBar/>
-    </>
+    <div className='App'>
+    <div className='side-guitar'>
+      <Guitar/>
+    </div>
+  <SideBar />
+  <div className='page'>
+    <span className='tags top-tags'>&lt;body&gt;</span>
+
+    <Outlet />
+
+    <span className='tags bottom-tags'>
+      &lt;/body&gt;
+      <br />
+      <span className='bottom-tag-html'>&lt;/html&gt;</span>
+      </span>
+    </div>
+  </div>
   );
 }
 
